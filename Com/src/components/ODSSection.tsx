2b97b0454f05030443f9350b6a0dd31ca36724a0
Hanzothe <gui_oliveirasantos123@hotmail.com>
2022-11-29 18:40:30 -0300

    Finished Prototype Visual

--- Com/src/components/ODSSection.tsx
+++ Com/src/components/ODSSection.tsx
@@ -12,52 +12,18 @@ function ODSSection() {
   const o4 = <img src={ODS4} width="500" alt="logo" />;
   const o5 = <img src={ODS5} width="500" alt="logo" />;
   const o10 = <img src={ODS10} width="500" alt="logo" />;
-  const carouselRef = React.useRef(null);
-  let resetTimeout;
-  const breakPoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 500, itemsToShow: 1 },
-    { width: 768, itemsToShow: 1 },
-    { width: 1200, itemsToShow: 1 },
-  ];
+
   return (
     <div className="ODSContainer">
-      <h1 class="place-self-center  font-sans font-bold text-6xl">
+      <h1 class="place-self-center font-extrabold text-6xl bg">
         Ajudamos empresas a atingir suas metas da ODS da ONU
       </h1>
-      <Carousel
-        breakPoints={breakPoints}
-        ref={carouselRef}
-        showArrows={true}
-        className="cursor-pointer"
-        enableAutoPlay
-        autoPlaySpeed={2000}
-        itemPadding={[0, 20]}
-        isRTL={false}
-        onNextEnd={({ index }) => {
-          if (
-            carouselRef?.current.state.activePage ===
-            carouselRef?.current.state.pages.length - 1
-          ) {
-            const itemsPerPage = Math.floor(
-              carouselRef?.current.props.children.length /
-                carouselRef?.current.getNumOfPages()
-            );
-
-            if (itemsPerPage === carouselRef?.current.state.activeIndex) {
-              clearTimeout(resetTimeout);
-              resetTimeout = setTimeout(() => {
-                carouselRef?.current?.goTo(0);
-              }, 2000); // same time
-            }
-          }
-        }}
-      >
-        <img src={ODS1} width="500" alt="logo" />
-        <img src={ODS4} width="500" alt="logo" />
-        <img src={ODS5} width="500" alt="logo" />
-        <img src={ODS10} width="500" alt="logo" />
-      </Carousel>
+      <div class="grid grid-flow-col justify-center gap-20">
+        <img src={ODS1} width="350" alt="logo" />
+        <img src={ODS4} width="350" alt="logo" />
+        <img src={ODS5} width="350" alt="logo" />
+        <img src={ODS10} width="350" alt="logo" />
+      </div>
     </div>
   );
 }
